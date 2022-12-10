d8b2c87d9d2d68fca7fb382e8ee83ae63ad7d699
cahya septia <cahyaseptia011@gmail.com>
2022-12-10 03:27:44 -0800

    exten report

--- src/test/java/org/example/CahyaDay15.java
+++ src/test/java/org/example/CahyaDay15.java
@@ -1,23 +1,63 @@
 package org.example;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import javafx.util.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
 
+
 public class CahyaDay15 {
+    public static ExtentTest test;
+    //builds a new report using the html template
+    ExtentHtmlReporter htmlReporter;
+    ExtentReports extent;
+    //helps to generate the logs in test report.
+
+
+    @BeforeTest
+    public void startReport() {
+        // initialize the HtmlReporter
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/test-output/testReport.html");
+
+        //initialize ExtentReports and attach the HtmlReporter
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        //configuration items to change the look and feel
+        //add content, manage tests etc
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("Extent Report Demo");
+        htmlReporter.config().setReportName("Test Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+    }
     @Test
     public void postTest(){
         System.setProperty("webdriver.chrome.driver", "C:\\sqa\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String baseURL = "https://demo.guru99.com/payment-gateway/index.php";
         driver.get(baseURL);
+        driver.manage().window().maximize();
 
         WebElement selectOption = driver.findElement(By.xpath("//select//option[@value='5']"));
         WebElement buyNow = driver.findElement(By.xpath("//input[@value='Buy Now']"));
@@ -46,9 +86,30 @@ public class CahyaDay15 {
 
         assertEquals(alert.getText(),expect);
 
+        test = extent.createTest("Payment Gateway", "PASSED test case");
+        Assert.assertTrue(true);
 
+        driver.quit();
     }
 
-//    private void asserEquals(String actual, String expect) {
-//    }
+    @AfterMethod
+    public void getResult(ITestResult result) {
+        if(result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
+            test.fail(result.getThrowable());
+        }
+        else if(result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" PASSED ", ExtentColor.GREEN));
+        }
+        else {
+            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" SKIPPED ", ExtentColor.ORANGE));
+            test.skip(result.getThrowable());
+        }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        //to write or update test information to reporter
+        extent.flush();
+    }
 }
